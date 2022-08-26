88748ee038b09c3a969dfe053f78b90d4678f97b
Luã Marcelo <luamarcelo17@gmail.com>
2022-08-25 21:05:19 -0300

    inversão de função

--- Exemplo1/lab12.py
+++ Exemplo1/lab12.py
@@ -1,19 +1,3 @@
-//comentário teste 1
-//comentário teste 2
-def desenhar(listaNum, maior):
-    for i in range(maior+2):
-        if i == 0 or i == (maior+1):
-            print('.'*(len(listaNum)+2))
-        else:
-            print('.', end='')
-            for j in range(len(listaNum)):
-                if i-1 + listaNum[j] >= maior:
-                    print('|', end='')
-                else:
-                    print(' ', end='')
-            print('.')
-
-
 def bublleSort(listaNum, maior):
     desenhar(listaNum, maior)
     troca = False
@@ -49,3 +33,16 @@ for n in listaStr:  # converte string em inteiro
         maior = num
 
 bublleSort(listaNum, maior)
+
+def desenhar(listaNum, maior):
+    for i in range(maior+2):
+        if i == 0 or i == (maior+1):
+            print('.'*(len(listaNum)+2))
+        else:
+            print('.', end='')
+            for j in range(len(listaNum)):
+                if i-1 + listaNum[j] >= maior:
+                    print('|', end='')
+                else:
+                    print(' ', end='')
+            print('.')
